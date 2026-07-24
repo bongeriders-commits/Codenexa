88ba12ebb2de9f54e1edb313f424fda1bfc86fbe
bongeriders-commits <bonge.riders@gmail.com>
2026-07-24 14:45:45 +0300

    Delete admin-logout.js

--- admin-logout.js
+++
@@ -1,7 +0,0 @@
-const { sendJson } = require('../lib/apiHelpers');
-const { buildClearCookie } = require('../lib/adminAuth');
-
-module.exports = async function handler(req, res) {
-  res.setHeader('Set-Cookie', buildClearCookie());
-  return sendJson(res, 200, { ok: true });
-};
